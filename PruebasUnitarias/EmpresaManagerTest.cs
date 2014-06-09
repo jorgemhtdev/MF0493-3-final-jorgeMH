01b0c8c9ce37b30c59b4d944dc09f750272fafad
jorgemhtdev <jorge@jorgemht.dev>
2014-06-09 15:18:13 +0200

    ExamenFinal

---
+++ PruebasUnitarias/EmpresaManagerTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MF0493_3.Models;
+
+namespace PruebasUnitarias
+{
+    /// <summary>
+    /// Summary description for EmpresaManagerTest
+    /// </summary>
+    [TestClass]
+    public class EmpresaManagerTest
+    {
+
+        [TestMethod]
+        public void NuevaTest()
+        {
+            // Comprobamos un objeto con datos
+
+            Empresa emp = new Empresa();
+
+            emp.activa = true;
+            emp.email = "aitortilla@gmail.com";
+            emp.ff = DateTime.Now;
+            emp.logo = "logo.png";
+            emp.nif = "75258966";
+            emp.nombre = "empresaPrueba";
+            emp.poblacion = "almeria";
+            emp.telefono = "6502824";
+            emp.usr = "admin";
+
+            EmpresaManager.Nueva(emp);
+
+            Empresa buscarEmpresa = EmpresaManager.get(emp.nif);
+
+            Assert.AreEqual(buscarEmpresa.email, "aitortilla@gmail.com");
+
+            
+            EmpresaManager.Eliminar(emp.nif);
+
+            // Comprobamos un objeto null
+
+            Empresa emp2 = new Empresa();
+
+            emp2 = null;
+
+            Assert.IsFalse(EmpresaManager.Nueva(emp2));
+
+        }
+
+        [TestMethod]
+         public void ModificarTest()
+        {
+            
+            Empresa emp = new Empresa();
+
+            emp.activa = true;
+            emp.email = "aitortilla@modificado.com";
+            emp.ff = DateTime.Now;
+            emp.logo = "modificado.png";
+            emp.nif = "75258962";
+            emp.nombre = "empresaPrueba";
+            emp.poblacion = "almeria";
+            emp.telefono = "6502824";
+            emp.usr = "admin";
+
+            EmpresaManager.Nueva(emp);
+
+            Empresa BuscarEmpresa = EmpresaManager.get(emp.nif);
+
+            Assert.AreEqual(BuscarEmpresa.email, "aitortilla@modificado.com");
+            Assert.AreEqual(BuscarEmpresa.logo, "modificado.png");
+
+            bool eliminado = EmpresaManager.Eliminar("75258962");
+
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            Empresa emp = new Empresa();
+            emp.activa = true;
+            emp.email = "aitortila@gmail.com";
+            emp.ff = DateTime.Now;
+            emp.logo = "logo.png";
+            emp.nif = "1234456789";
+            emp.nombre = "aitortila";
+            emp.poblacion = "aitortilaAlmeria";
+            emp.telefono = "1233456789";
+            emp.usr = "admin";
+
+            Assert.IsNull(EmpresaManager.get("0380932"));
+
+            EmpresaManager.Nueva(emp);
+
+            Empresa buscar = EmpresaManager.get(emp.nif);
+
+            Assert.AreEqual(buscar.nombre, "aitortila");
+
+        }
+        [TestMethod]
+        public void EliminarTest()
+        {
+            bool eliminado = EmpresaManager.Eliminar("1234456789");
+            Assert.IsTrue(eliminado);
+        }
+    }
+}
---
+++ PruebasUnitarias/UsuarioTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MF0493_3.Models;
+
+namespace PruebasUnitarias
+{
+    /// <summary>
+    /// Summary description for Usuario
+    /// </summary>
+    [TestClass]
+    public class UsuarioTest
+    {
+        // No te molestes en copiarme, hasta ultima hora no voy subir al repositorio este codigo pillin
+
+        [TestMethod]
+        public void validarTest()
+        {
+            Usuario user = new Usuario();
+            user.passwdSinCifrar = "123123";
+
+            bool cifrada = user.validar("123123");
+
+            Assert.IsTrue(cifrada);
+        }
+    }
+}
