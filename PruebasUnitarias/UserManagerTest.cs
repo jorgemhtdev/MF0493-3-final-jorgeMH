2908b42ca1598c70df46e50ba11dc4200d1d4103
jorgemht <jorge@jorgemht.com>
2014-06-09 15:18:13 +0200

    ExamenFinal

---
+++ PruebasUnitarias/UserManagerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MF0493_3.Models;
+
+namespace PruebasUnitarias
+{
+    /// <summary>
+    /// Summary description for UserManagerTest
+    /// </summary>
+    [TestClass]
+    public class UserManagerTest
+    {
+        // No te molestes en copiarme, hasta ultima hora no voy subir al repositorio este codigo pillin
+
+        [TestMethod]
+        public void getAllTest()
+        {
+            List<Usuario> list = new List<Usuario>();
+            list = UserManager.getAll();
+            Assert.AreEqual(list.Count, 1);
+        }
+
+        [TestMethod]
+        public void CrearAdminTest()
+        {
+            UserManager.CrearAdmin();
+
+            Usuario user = UserManager.get("admin");
+            Assert.AreEqual(user.username, "admin");
+            Assert.AreEqual(user.email, "jucles@a2000.es");
+        }
+    }
+}
---
+++ UnitTestProject1/UserManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    class UserManagerTest
+    {
+        [TestMethod]
+        public void getAllTest()
+        {
+            UserManager curso = new UserManager();
+
+            List<UserManager> lista = curso.getAll();
+
+            Assert.AreEqual(lista.Count, 2);
+
+        }
+    }
+}
